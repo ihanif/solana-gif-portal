6b3ecc8b2d59edac963178ebce7a229595209468
Hanif U Rahman <hanif@Hanifs-MacBook-Air.local>
2021-11-29 00:34:08 +0000

    Fix linting errors
    Vercel ci build fails because it treats warnings and linting errors as failures. This commit rmoves unused variables and fixes other similar liniting errors

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import idl from "./idl.json";
 import kp from "./keypair.json";
 
 // SystemProgram is a reference to the Solana runtime!
-const { SystemProgram, Keypair } = web3;
+const { SystemProgram } = web3;
 
 // Create a keypair for the account that will hold the GIF data.
 const arr = Object.values(kp._keypair.secretKey);
@@ -30,12 +30,12 @@ const opts = {
 const TWITTER_HANDLE = "_buildspace";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
-const TEST_GIFS = [
+/* const TEST_GIFS = [
   "https://i.giphy.com/media/eIG0HfouRQJQr1wBzz/giphy.webp",
   "https://media3.giphy.com/media/L71a8LW2UrKwPaWNYM/giphy.gif?cid=ecf05e47rr9qizx2msjucl1xyvuu47d7kf25tqt2lvo024uo&rid=giphy.gif&ct=g",
   "https://media4.giphy.com/media/AeFmQjHMtEySooOc8K/giphy.gif?cid=ecf05e47qdzhdma2y3ugn32lkgi972z9mpfzocjj6z1ro4ec&rid=giphy.gif&ct=g",
   "https://i.giphy.com/media/PAqjdPkJLDsmBRSYUp/giphy.webp",
-];
+]; */
 
 const App = () => {
   // State
@@ -239,7 +239,7 @@ const App = () => {
       console.log("Fetching GIF list...");
       getGifList();
     }
-  }, [walletAddress]);
+  });
 
   return (
     <div className="App">
